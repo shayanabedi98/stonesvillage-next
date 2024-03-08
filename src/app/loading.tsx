e5e935e871575ce0213a944f43a68f84da93cdb0
shayanabedi98 <shayanabedi@hotmail.com>
2024-03-07 23:18:08 -0500

    add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center animate-spin rounded-full p-10 h-40 w-40 border-t-2 border-b-2 border-accent-color">
+        <Image height={200} width={200} src="/assets/other/sv-cube.png" alt="" />
+      </div>
+    </div>
+  );
+}
